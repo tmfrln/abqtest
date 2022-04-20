cc7d4f4c0b46796b96d29a85ac7a6a0c6cc5a0f1
Tim Furlan <tim.furlan@tu-dortmund.de>
2022-04-20 12:41:05 +0200

    Numpy style docstrings in main.py

--- main.py
+++ main.py
@@ -8,19 +8,49 @@ from abaqus import session
 class odb(object):
     """
     A wrapper for odbs.
+
+    Attributes
+    ----------
+    path : str
+        Path to the underlying odb.
+    odb : Abaqus odb object or None
+        The open odb, if one is open.
+
     """
-    
+
     def __init__(self, path):
         self.path = path
         self.odb = None
-        
+
     def open(self):
-        """Open an odb. """
+        """
+        Open an odb.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        odb : Abaqus odb object.
+
+        """
         self.odb = session.openOdb(self.path)
         return self.odb
-    
+
     def close(self):
-        """Close the odb."""
+        """
+        Close the odb.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        """
         self.odb.close()
         self.odb = None
-        
+
